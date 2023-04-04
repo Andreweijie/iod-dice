f1465c6d6f7480c950491d8e8f3643818de16218
Andre Goh <andregoh1996@gmail.com>
2023-04-05 00:27:29 +0800

    added tests

--- js/index.js
+++ js/index.js
@@ -1,9 +1,38 @@
 function generateResult(sides) {
-    return Math.floor(Math.random() * (sides - 1 + 1) + 1)
+  return Math.floor(Math.random() * (sides - 1 + 1) + 1);
 }
 
-function rollDie(sides) {
-    let result = generateResult(sides)
+function displayResult(result) {
+  document.getElementById("result").innerText = result;
+}
 
-    document.getElementById('result').innerText = result
+function rollDie() {
+  let currentlySelectedNumberOfSides =
+    document.getElementById("numberOfSides").value;
+
+  let result = generateResult(currentlySelectedNumberOfSides);
+
+  displayResult(result);
+}
+
+/* tests */
+
+let exampleResult = generateResult(10);
+
+if (exampleResult <= 10 && exampleResult >= 1) {
+  console.log("generate test passed!");
+} else {
+  throw new Error(
+    `expected number to be between 1 and 10, instead got: ${exampleResult}`
+  );
+}
+
+displayResult("0");
+
+let currentResult = document.getElementById("result").innerText;
+
+if (currentResult == "0") {
+  console.log("display test passed!");
+} else {
+  throw new Error(`expected 0 but got: ${currentResult}`);
 }
